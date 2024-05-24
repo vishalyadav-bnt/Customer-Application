028dd5b69a9dcacc2bda3d78f7c39ea96511bee6
Vishal Yadav <vishal.yadav@bnt.local>
2024-05-24 10:43:10 +0530

    Some Changes in Controller

--- bnt/src/main/java/com/example/bnt/controller/CustomerController.java
+++ bnt/src/main/java/com/example/bnt/controller/CustomerController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,8 +43,8 @@ public class CustomerController {
 
     }
 
-    @PutMapping
-    public CustomerModel updateData(@RequestParam("id") int id, @RequestParam("name") String name)
+    @PutMapping("/{id}")
+    public CustomerModel updateData(@PathVariable("id") int id, @RequestParam("name") String name)
             throws DataIsNotPresent {
         if (!customerservice.getId().contains(id)) {
             throw new DataIsNotPresent("Data Is Not Present");
@@ -51,8 +52,8 @@ public class CustomerController {
         return customerservice.updateData(id, name);
     }
 
-    @DeleteMapping
-    public ResponseEntity<String> deleteData(@RequestParam("id") int id) throws DataIsNotPresent {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteData(@PathVariable("id") int id) throws DataIsNotPresent {
         if (!customerservice.getId().contains(id)) {
             throw new DataIsNotPresent("Data Is Not Present");
         }
